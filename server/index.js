61da4b3dceef4b3271c55c606d2df785401d6d11
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-01-31 22:15:24 +0530

    added package.json

--- server/index.js
+++ server/index.js
@@ -1,1 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://123Prashant:Shishir@123@cluster0.cjm17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+//Returns a promise
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port:${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
+
 // mongodb+srv://<username>:<password>@cluster0.cjm17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
